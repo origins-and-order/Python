439c61471f2c9de454fe85eb8eafd607497a6d60
Dustin Ramsey <dustin.ramsey@outlook.com>
2016-10-19 16:56:38 -0500

    edited some stuff...

--- crawler/test.py
+++ crawler/test.py
@@ -8,18 +8,3 @@ spidey = Crawler.Crawler()
 
 # lil spidey :)
 spidey.search(url)
-
-# okay, get the exterminator lmao
-spidey.concurrent_search(url, 2048)
-
-# rank how kool the pages r ;-O
-# spidey.ranker(url)
-
-
-# def test(container):
-#     for i in range(10):
-#         container.add(i)
-#
-# c = set()
-# test(c)
-# print c
